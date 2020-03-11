ebdd1894075b5dd42106756fd890e449ba76ecdc
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-11 13:24:21 -0400

    webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  "mode": "none",
+  "entry": "./src/index.js",
+  "output": {
+    "path": __dirname + '/dist',
+    "filename": "bundle.js"
+  },
+  "module": {
+    "rules": [
+      {
+        "test": /\.js$/,
+        "exclude": /node_modules/,
+        "use": {
+          "loader": "babel-loader",
+          "options": {
+            "presets": [
+              "@babel/preset-env",
+            ]
+          }
+        }
+      },
+      {
+        "test": /\.css$/,
+        "use": [
+          "style-loader",
+          "css-loader"
+        ]
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist')
+  }
+};
