610e67142357c0c4019663542dec2c76cc24e0a4
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-11 14:33:07 -0400

    create new Game

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './styles/styles.css';
+import Game from './game';
+
+const ctx = document.getElementById('screen').getContext('2d');
+const WIDTH = 800, HEIGHT = 600;
+const game = new Game(WIDTH, HEIGHT);
