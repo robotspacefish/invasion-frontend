7f77477d2a200fba5b6ac571f850f91a4859ed71
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-12 13:11:42 -0400

    create player & add player draw method

--- src/game.js
+++ src/game.js
@@ -1,14 +1,18 @@
+import Player from './player';
+import { spritesheet } from './assets/invasion_sheet.png';
 export default class Game {
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.player = new Player();
   }
+
 
   update() {
 
   }
 
-  draw() {
-
+  draw(ctx) {
+    this.player.draw(ctx);
   }
 }
