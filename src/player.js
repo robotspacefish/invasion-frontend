759b38b66998d6d5a1fa87b9765765ce411dee15
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-12 13:11:52 -0400

    draw player sprite

--- src/player.js
+++ src/player.js
@@ -1,9 +1,19 @@
-class Player {
-  constructor() {
+import SpriteObject from './spriteObject';
+import Spritesheet from './assets/invasion_sheet.png';
+import { GAME_WIDTH, GAME_HEIGHT } from './index';
 
+export default class Player {
+  constructor() {
+    this.spriteObj = this.initSprite();
   }
 
-  draw() {
+  initSprite() {
+    return new SpriteObject(3, 0, 218, 164, 0, 0, 218 / 2, 164 / 2, Spritesheet);
+  }
+
+  draw(ctx) {
+    const { sourceX, sourceY, sourceWidth, sourceHeight, x, y, width, height, image } = this.spriteObj;
+    ctx.drawImage(image, sourceX, sourceY, sourceWidth, sourceHeight, x, y, width, height);
 
   }
 
