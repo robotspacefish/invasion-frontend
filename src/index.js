f11f70ec0420f5c7b02ac616c97d6d90e0b626d2
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-12 13:11:06 -0400

    game loop

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@ import './styles/styles.css';
 import Game from './game';
 
 const ctx = document.getElementById('screen').getContext('2d');
-const WIDTH = 800, HEIGHT = 600;
-const game = new Game(WIDTH, HEIGHT);
+const GAME_WIDTH = 800, GAME_HEIGHT = 600;
+const game = new Game(GAME_WIDTH, GAME_HEIGHT);
+let lastTime = 0;
+
+function gameLoop(timeStamp) {
+  let delta = timeStamp - lastTime;
+  lastTime = timeStamp;
+
+  ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+  game.draw(ctx);
+  game.update();
+
+  requestAnimationFrame(gameLoop);
+}
+
+requestAnimationFrame(gameLoop);
+
+export { GAME_WIDTH, GAME_HEIGHT };
