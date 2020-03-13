71ae0eca339673f6815df927fbe30411afd93cf3
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-13 19:53:21 -0400

    initObj method for enemy properties to be destructured

--- src/enemy.js
+++ src/enemy.js
@@ -3,13 +3,24 @@ import SpriteObject from "./spriteObject";
 import { GAME_WIDTH, GAME_HEIGHT } from './index';
 import { generateRandomNumber } from './utils';
 
-import Game from "./game";
-
 export default class Enemy extends GameObject {
   constructor(speed = 0.45) {
     super();
-    this.spriteObj = new SpriteObject(0, 176, 218, 169, generateRandomNumber(0, GAME_WIDTH - 169), -200, 218 / 2, 169 / 2);
+    this.spriteObj = new SpriteObject(Enemy.initObj());
     this.speed = speed;
+  }
+
+  static initObj() {
+    return {
+      sourceX: 0,
+      sourceY: 176,
+      sourceWidth: 218,
+      sourceHeight: 169,
+      x: generateRandomNumber(0, GAME_WIDTH - 169),
+      y: -200,
+      width: 218 / 2,
+      height: 169 / 2
+    }
   }
 
   static get frequency() {
