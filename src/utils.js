4deca4c808a30c95fbe06cf1e86c8acc80cad548
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-13 10:50:13 -0400

    generateRandomNumber

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,9 @@
 function mid(min, max, num) {
+const generateRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+const mid = (min, max, num) => {
   if (num >= max) return max;
   if (num <= min) return min;
   return num;
