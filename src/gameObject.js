f77668db71b9da89c198e817427ede42b6a25697
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-12 23:18:40 -0400

    remove objects from all

--- src/gameObject.js
+++ src/gameObject.js
@@ -3,6 +3,15 @@ export default class GameObject {
   constructor() {
     GameObject.all.push(this);
   }
+
+  static remove(obj) {
+    GameObject.all = GameObject.all.filter(gameObj => {
+      return gameObj.spriteObj.x !== obj.spriteObj.x &&
+        gameObj.spriteObj.y !== obj.spriteObj.y;
+    });
+
+  }
+
   draw(ctx) {
     const { sourceX, sourceY, sourceWidth, sourceHeight, x, y, width, height, image } = this.spriteObj;
     ctx.drawImage(image, sourceX, sourceY, sourceWidth, sourceHeight, x, y, width, height);
