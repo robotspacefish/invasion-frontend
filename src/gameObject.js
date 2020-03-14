e2cb07d7271ab620902e5cc20a3ce90cd9a7a28f
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-13 23:23:31 -0400

    has collided & can collide

--- src/gameObject.js
+++ src/gameObject.js
@@ -9,7 +9,23 @@ export default class GameObject {
     GameObject.all = GameObject.all.filter(gameObj => {
       return JSON.stringify(gameObj) !== JSON.stringify(objToDelete);
     });
+  }
 
+  hasCollided(obj) {
+    const { x, y, width, height } = this.spriteObj;
+    return x + width > obj.x &&
+      x < obj.x + obj.width &&
+      y < obj.y + obj.height &&
+      y + height > obj.y;
+  }
+
+  canCollide(obj) {
+    const pType = this.type, oType = obj.type;
+
+    return (pType === "player" && oType === "enemy") ||
+      (pType === "enemy" && oType === "player") ||
+      (pType === "player" && oType === "enemyBullet") ||
+      (pType === "playerBullet" && oType === "enemy")
   }
 
   draw(ctx) {
