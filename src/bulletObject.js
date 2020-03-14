6a12a006b9d4b6701e8ad1c5720e8ec97eca6cda
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-13 21:10:21 -0400

    Player bullet object

--- src/bulletObject.js
+++ src/bulletObject.js
@@ -1,21 +1,54 @@
 import GameObject from './gameObject';
-import SpriteObject from './spriteObject';
-import { GAME_WIDTH, GAME_HEIGHT } from './index';
-import GameObject from './gameObject';
+import AnimatedSpriteObject from './animatedSpriteObject';
+import waves from './assets/waves.png';
 
-class BulletObject extends GameObject {
-  constructor(type, playerObj) {
+export default class BulletObject extends GameObject {
+  constructor(type, shooter) {
     super();
-    this.spriteObj = this.init(type)
-    BulletObject.all.push(this);
+    this.type = type;
+    this.spriteObj = this.init(shooter)
+    // BulletObject.all.push(this);
   }
 
-  init() {
-    new SpriteObject(448, 0, 32, 304, playerObj.width / 2, playerObj)
+  init(shooter) {
+    let obj = {};
+    if (this.type === "player") {
+      obj = {
+        sourceX: 0,
+        sourceY: 0,
+        sourceWidth: 224,
+        sourceHeight: 344,
+        width: 224 / 2,
+        height: 344 / 2,
+        frameCount: 12,
+        columns: 4,
+        rows: 3,
+        sheetWidth: 1032,
+        sheetHeight: 896,
+        spritesheet: waves,
+        x: shooter.x + (shooter.width / 2),
+        y: shooter.y
+      }
+    }
+    // else if (type === "enemy") {
+
+    // }
+    return new AnimatedSpriteObject(obj)
+  }
+
+  static speed() {
+    return 2;
   }
 
   update() {
+    if (this.type === "player") {
+      this.x -= BulletObject.speed;
+    }
+    this.spriteObj.animate();
 
+    if (this.spriteObj.currentFrame === this.spriteObj.frameCount) {
+      GameObject.remove(this);
+    }
   }
 
 }
