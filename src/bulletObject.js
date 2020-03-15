67a76abf9cb889470e4b369b2b49b7f6fe0e1107
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-14 23:08:08 -0400

    use waves2 for playerBullet & adjust properties

--- src/bulletObject.js
+++ src/bulletObject.js
@@ -1,11 +1,13 @@
 import GameObject from './gameObject';
 import AnimatedSpriteObject from './animatedSpriteObject';
 import waves from './assets/waves.png';
+import waves2 from './assets/waves2.png';
 
 export default class BulletObject extends GameObject {
-  constructor(type, shooter) {
+  constructor(type, speed, shooter) {
     super();
     this.type = type;
+    this.speed = speed;
     this.spriteObj = this.init(shooter)
   }
 
@@ -15,18 +17,19 @@ export default class BulletObject extends GameObject {
       obj = {
         sourceX: 0,
         sourceY: 0,
-        sourceWidth: 224,
-        sourceHeight: 344,
-        width: 224 / 2,
-        height: 344 / 2,
-        frameCount: 12,
-        columns: 4,
-        rows: 3,
-        sheetWidth: 1032,
-        sheetHeight: 896,
-        spritesheet: waves,
+        sourceWidth: 228,
+        sourceHeight: 79,
+        width: 228 / 2,
+        height: 79 / 2,
+        frameCount: 7,
+        columns: 1,
+        rows: 7,
+        sheetWidth: 228,
+        sheetHeight: 533,
+        spritesheet: waves2,
         x: shooter.x + 224 / 2 - shooter.width,
-        y: 450 - 344 / 2
+        // y: 450 - 344 / 2
+        y: 400
       }
     }
     // else if (type === "enemy") {
@@ -41,8 +44,9 @@ export default class BulletObject extends GameObject {
 
   update() {
     super.update();
-    if (this.type === "playerBullets") {
-      this.x -= BulletObject.speed;
+    if (this.type === "playerBullet") {
+
+      this.spriteObj.y -= this.speed
     }
     this.spriteObj.animate();
 
