5206b2108205f48866ef85675dcecca9c5b1462a
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-15 16:48:10 -0400

    add conditionals based on game modes

--- src/index.js
+++ src/index.js
@@ -1,30 +1,43 @@
 import './styles/styles.css';
 import Game from './game';
-const ctx = document.getElementById('screen').getContext('2d');
 const GAME_WIDTH = 800, GAME_HEIGHT = 600;
 const game = new Game(GAME_WIDTH, GAME_HEIGHT);
 
 let lastTime = 0;
 
 function gameLoop(timeStamp) {
+
   let deltaTime = timeStamp - lastTime;
   lastTime = timeStamp;
 
-  ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
-  game.draw(ctx);
-  game.update(deltaTime);
+  if (game.mode === "play") {
+    game.draw();
+    game.update(deltaTime);
+  }
 
   requestAnimationFrame(gameLoop);
 }
 
 window.addEventListener('keydown', (e) => {
-  if (e.key === 'ArrowRight') game.player.moveRight = true;
-  if (e.key === 'ArrowLeft') game.player.moveLeft = true;
-  if (e.code === "Space") game.player.shoot = true;
+  if (game.mode === "title") {
+    if (e.code === "Space") {
+      game.mode = "play";
+      game.renderCanvas();
+    }
+  } else if (game.mode === "play") {
+    if (e.key === 'ArrowRight') game.player.moveRight = true;
+    if (e.key === 'ArrowLeft') game.player.moveLeft = true;
+    if (e.code === "Space") game.player.shoot = true;
+  }
 
   e.preventDefault(); // prevent scrolling browser
 });
 
-requestAnimationFrame(gameLoop);
+function start() {
+  game.screens.title.render();
+  requestAnimationFrame(gameLoop);
+}
 
+
+start();
 export { GAME_WIDTH, GAME_HEIGHT };
