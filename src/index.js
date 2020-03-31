0e4c70c2444a2462be0e39a079b3820d80b0f77b
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-31 15:03:03 -0400

    cleanup organization

--- src/index.js
+++ src/index.js
@@ -1,33 +1,19 @@
-import './styles/styles.css';
 import UsersAdapter from './user/usersAdapter';
 import LeaderboardAdapter from './leaderboard/leaderboardAdapter';
 import Game from './game';
 import GameObject from './gameObject';
+import './styles/styles.css';
 
 const usersScoreDiv = document.getElementById('users-scores');
 const leaderboardDiv = document.getElementById('leaderboard-scores');
 
 const GAME_WIDTH = 800, GAME_HEIGHT = 500;
 let game;
-let lastTime = 0;
 
 function handleFormSubmit(e, score) {
   e.preventDefault();
   const usernameFromInput = e.target.parentNode.children[0].value.toLowerCase();
   if (usernameFromInput !== "") UsersAdapter.addNewUser(usernameFromInput, score);
-}
-
-function gameLoop() {
-  if (game.mode === "gameOver") {
-    game.screens.gameOver()
-  } else {
-    if (game.mode === "play") {
-      game.draw();
-      game.update();
-    }
-
-    requestAnimationFrame(gameLoop);
-  }
 }
 
 window.addEventListener('keydown', (e) => {
@@ -53,11 +39,24 @@ function init() {
   requestAnimationFrame(gameLoop);
 }
 
-function start() {
-  fetchData(leaderboardDiv, usersScoreDiv);
-  init();
-  game.screens.title();
+function fetchData(leaderboardDiv, usersScoreDiv) {
+  UsersAdapter.fetchUsers(usersScoreDiv);
+  LeaderboardAdapter.fetchLeaderboard(leaderboardDiv);
+}
 
+function gameLoop() {
+  // continues to loop if mode !== gameOver
+
+  if (game.mode === "gameOver") {
+    game.screens.gameOver()
+  } else {
+    if (game.mode === "play") {
+      game.draw();
+      game.update();
+    }
+
+    requestAnimationFrame(gameLoop);
+  }
 }
 
 function reset() {
@@ -66,11 +65,18 @@ function reset() {
   game.renderCanvas();
 }
 
-function fetchData(leaderboardDiv, usersScoreDiv) {
-  LeaderboardAdapter.fetchLeaderboard(leaderboardDiv);
-  UsersAdapter.fetchUsers(usersScoreDiv);
+function start() {
+  fetchData(leaderboardDiv, usersScoreDiv);
+  init();
+  game.screens.title();
+
 }
 
 start();
+
+const btn = document.getElementById('sort-btn');
+btn.addEventListener('click', () => {
+  UsersAdapter.sortUserByName(document.getElementById('sorted-users'));
+})
 
 export { GAME_WIDTH, GAME_HEIGHT, start, reset, handleFormSubmit, usersScoreDiv, leaderboardDiv };
