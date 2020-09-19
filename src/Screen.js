ad6d6161c88aad0a40b1eb946b8ce3aaeda2a05e
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-09-19 19:25:20 -0400

    display highest scores

--- src/Screen.js
+++ src/Screen.js
@@ -34,29 +34,57 @@ export default class Screen {
     `
   }
 
-  static renderGameOver(finalScore, finalWave) {
-    container.innerHTML = `
-      <div id="game-over-screen">
+  static renderGameOver(finalScore, finalWave, highestScores) {
+    const scores = highestScores.map((score, i) => {
+      return `<tr><td>${i + 1}</td><td>${score.username}</td><td>${score.value}</td></tr>`;
+    }).join("");
+
+    const isFormVisible = finalScore > highestScores[highestScores.length - 1].value;
+
+    const content = `
         <h1>Game Over!</h1>
         <div id="finals">
           <p id="final-score">Score: <span>${finalScore}</span></p>
           <p id="final-wave">Wave: <span>${finalWave}</span></p>
         </div>
 
+        <table id="high-scores">
+          <thead>
+            <tr>
+              <td>RANK</td>
+              <td>NAME</td>
+              <td>SCORE</td>
+            </tr>
+          </thead>
+          <tbody>
+            ${scores}
+          </tbody>
+        </table>
+
         <div id="game-over-input">
-          <form>
-            <label>Enter Your Name</label></br>
-           <div>
-              <input type="text">
-              <button id="submit-score-btn" class="btn">Enter</button>
-           </div>
-          </form
+          ${isFormVisible ? addScoreForm() : ''}
+          <button id="play-again-btn" class="btn">Play Again?</button>
         </div>
-        <button id="play-again-btn" class="btn">Play Again?</button>
-      </div>
     `;
 
-    document.getElementById('submit-score-btn').addEventListener('click', (e) => handleFormSubmit(e, finalScore));
+    container.innerHTML = content;
+
+    // event listeners
+    isFormVisible ? document.getElementById('submit-score-btn').addEventListener('click', (e) => handleFormSubmit(e, finalScore)) : null;
+
     document.getElementById('play-again-btn').addEventListener('click', reset)
   }
+}
+
+function addScoreForm() {
+  return `
+    <form>
+      <label>Enter Your Name</label></br>
+      <div>
+        <input type="text">
+        <button id="submit-score-btn" class="btn">Enter</button>
+      </div>
+    </form
+  </div>
+  `;
 }
