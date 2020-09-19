978c2fe4766afd58c09696dc8d03cedde3595ad0
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-09-19 19:24:53 -0400

    remove id that wasnt in use & return promise

--- src/leaderboard/leaderboardAdapter.js
+++ src/leaderboard/leaderboardAdapter.js
@@ -2,19 +2,20 @@ import { BASE_URL, resToJson } from '../utils';
 import Leaderboard from './leaderboard';
 
 export default class LeaderboardAdapter {
-  static fetchLeaderboard(container) {
-    fetch(`${BASE_URL}/leaderboard`)
+  static fetchLeaderboard() {
+    return fetch(`${BASE_URL}/leaderboard`)
       .then(resToJson)
       .then(json => {
         const highestScoringUsers = json.map(s => {
           return {
-            id: s.id,
             value: s.value,
             username: s.username
           };
         });
+
         const leaderboard = new Leaderboard(highestScoringUsers);
-        leaderboard.render(container);
+        // leaderboard.render(container);
+        return leaderboard;
       })
       .catch(error => console.error(error.message));
   }
